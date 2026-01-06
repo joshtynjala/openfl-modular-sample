36e0905907861082e056bbf1df0136fd71dbec9d
Josh Tynjala <joshtynjala@bowlerhat.dev>
2026-01-06 09:42:19 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Compile OpenFL with haxe-modular Sample
+
+Compiles an [OpenFL](https://openfl.org/) project using the [haxe-modular](https://lib.haxe.org/p/modular) library. When a button is clicked, a new module is loaded from the server at runtime.
+
+haxe-modular modifies the behavior of the Haxe compiler to split the generated JavaScript into separate modules when the `Bundle.load()` method is used.
+
+Sample created by [Josh Tynjala](https://twitter.com/joshtynjala), the author of [Feathers UI](https://feathersui.com/).
+
+## Usage
+
+Add the [modular](https://lib.haxe.org/p/modular) Haxelib to your _project.xml_ file:
+
+```xml
+<haxelib name="modular"/>
+```
+
+Run the following commands in your project directory:
+
+```sh
+npm init -y
+npm install haxe-modular --save-dev
+```
+
+That's it! You don't need to do anything special to build and run.
+
+```sh
+lime test html5 -debug
+```
+
+## Requirements
+
+- haxe-modular 0.14.0 or newer
+- Lime 8.0.0 or newer
+- OpenFL 9.2.0 or newer
+
+## Credits
+
+Sample created by [Josh Tynjala](https://github.com/sponsors/joshtynjala), the author of [Feathers UI](https://feathersui.com/) and a member of the [OpenFL](https://openfl.org/) leadership team.
---
+++ src/Main.hx
@@ -0,0 +1,53 @@
+import com.example.MyModule;
+import openfl.display.Sprite;
+import openfl.events.MouseEvent;
+import openfl.text.TextField;
+import openfl.text.TextFormat;
+
+class Main extends Sprite {
+	private var button:Sprite;
+
+	public function new() {
+		super();
+
+		var tf = new TextField();
+		tf.defaultTextFormat = new TextFormat("_sans", 20, 0x666666);
+		tf.htmlText = "Hello from <b>OpenFL</b> and <b>haxe-modular</b>";
+		tf.autoSize = LEFT;
+		tf.x = 10.0;
+		tf.y = 10.0;
+		addChild(tf);
+
+		button = new Sprite();
+		button.buttonMode = true;
+		button.mouseChildren = false;
+		button.graphics.beginFill(0xff0000);
+		button.graphics.drawRoundRect(0.0, 0.0, 150.0, 50.0, 10.0);
+		button.graphics.endFill();
+		var buttonText = new TextField();
+		buttonText.defaultTextFormat = new TextFormat("_sans", 20, 0xffffff);
+		buttonText.autoSize = LEFT;
+		buttonText.text = "Load Module";
+		buttonText.x = (button.width - buttonText.width) / 2.0;
+		buttonText.y = (button.height - buttonText.height) / 2.0;
+		button.x = 10.0;
+		button.y = 50.0;
+		button.addChild(buttonText);
+		addChild(button);
+
+		button.addEventListener(MouseEvent.CLICK, onButtonClick);
+	}
+
+	private function onButtonClick(event:MouseEvent):Void {
+		button.mouseEnabled = false;
+		button.alpha = 0.5;
+
+		// load the module on demand
+		Bundle.load(MyModule).then(function(_):Void {
+			var module = new MyModule();
+			module.x = 10.0;
+			module.y = 120.0;
+			addChild(module);
+		});
+	}
+}
